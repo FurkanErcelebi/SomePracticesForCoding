41154138011388bc351156eab201432dc995c1b4
IBR41Furkan <48163755+FurkanErcelebi@users.noreply.github.com>
2021-04-28 21:17:41 +0300

    Create WeatherObservationStation13.sql

---
+++ hackerrank/SQL/WeatherObservationStation13.sql
@@ -0,0 +1,9 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+select sum(lat_n)
+from station
+where lat_n > 38.7880
+    and lat_n < 137.2345
+order by lat_n;
